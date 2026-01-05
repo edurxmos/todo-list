8103775b3c8dd0e5fcc1f2eddad76965bccb8ee1
edurxmos <eduardoramos15975@gmail.com>
2026-01-05 20:54:50 -0300

    entity Task

---
+++ src/main/java/com/eduardo/todo_list/entities/Task.java
@@ -0,0 +1,32 @@
+package com.eduardo.todo_list.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Instant;
+
+@NoArgsConstructor
+@Getter
+@EqualsAndHashCode(of = "id")
+
+@Entity
+@Table(name = "tb_task")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private boolean done;
+
+    @Column(nullable = false)
+    private Instant createdAt;
+
+}
