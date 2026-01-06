0607d616e73637bdea34dae5cb0b6b244f26649e
edurxmos <eduardoramos15975@gmail.com>
2026-01-05 21:02:20 -0300

    feat: TaskService and CRUD

---
+++ src/main/java/com/eduardo/todo_list/services/TaskService.java
@@ -0,0 +1,39 @@
+package com.eduardo.todo_list.services;
+
+import com.eduardo.todo_list.entities.Task;
+import com.eduardo.todo_list.repositories.TaskRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+
+    private TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public List<Task> list() {
+        return taskRepository.findAll();
+    }
+
+    public List<Task> create(Task task) {
+        taskRepository.save(task);
+        return list();
+    }
+
+    public List<Task> update(Task task) {
+        taskRepository.save(task);
+        return list();
+    }
+
+    public List<Task> delete(Long id) {
+        taskRepository.deleteById(id);
+        return list();
+    }
+
+}
