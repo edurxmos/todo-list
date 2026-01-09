91f67a8cb6533baea9e637b3627a961c378f9d3f
edurxmos <eduardoramos15975@gmail.com>
2026-01-08 22:45:30 -0300

    feat: add ResponseEntity in endpoints

--- src/main/java/com/eduardo/todo_list/controllers/TaskController.java
+++ src/main/java/com/eduardo/todo_list/controllers/TaskController.java
@@ -5,6 +5,8 @@ import com.eduardo.todo_list.dtos.TaskResponseDTO;
 import com.eduardo.todo_list.services.TaskService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,28 +20,28 @@ public class TaskController {
     }
 
     @GetMapping
-    public Page<TaskResponseDTO> list(Pageable pageable) {
-        return taskService.list(pageable);
+    public ResponseEntity<Page<TaskResponseDTO>> list(Pageable pageable) {
+        return ResponseEntity.ok(taskService.list(pageable));
     }
 
     @PostMapping
-    public Page<TaskResponseDTO> create(@RequestBody TaskRequestDTO dto, Pageable pageable) {
-        return taskService.create(dto, pageable);
+    public ResponseEntity<Page<TaskResponseDTO>> create(@RequestBody TaskRequestDTO dto, Pageable pageable) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(taskService.create(dto, pageable));
     }
 
     @PutMapping("/{id}")
-    public Page<TaskResponseDTO> update(@PathVariable Long id, @RequestBody TaskRequestDTO dto, Pageable pageable) {
-        return taskService.update(id, dto, pageable);
+    public ResponseEntity<Page<TaskResponseDTO>> update(@PathVariable Long id, @RequestBody TaskRequestDTO dto, Pageable pageable) {
+        return ResponseEntity.ok(taskService.update(id, dto, pageable));
     }
 
     @DeleteMapping("/{id}")
-    public Page<TaskResponseDTO> delete(@PathVariable Long id, Pageable pageable) {
-        return taskService.delete(id, pageable);
+    public ResponseEntity<Page<TaskResponseDTO>> delete(@PathVariable Long id, Pageable pageable) {
+        return ResponseEntity.ok(taskService.delete(id, pageable));
     }
 
     @PutMapping("/done/{id}")
-    public Page<TaskResponseDTO> markAsDone(@PathVariable Long id, Pageable pageable) {
-        return taskService.markAsDone(id, pageable);
+    public ResponseEntity<Page<TaskResponseDTO>> markAsDone(@PathVariable Long id, Pageable pageable) {
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(taskService.markAsDone(id, pageable));
     }
 
 }
